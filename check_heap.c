7d38f5ab6087c0e524e12c9147493233e0abee26
Jaimie Ren <jaimier@cs.utexas.edu>
2023-09-20 09:16:21 -0500

    issues with creating a memory_block_t

--- check_heap.c
+++ check_heap.c
@@ -1,5 +1,7 @@
 
 #include "umalloc.h"
+#include <stdio.h>
+#include <stdint.h>
 
 //Place any variables needed here from umalloc.c as an extern.
 extern memory_block_t *free_head;
@@ -26,13 +28,29 @@ int check_heap() {
     // Example heap check:
     // Check that all blocks in the free list are marked free.
     // If a block is marked allocated, return -1.
-    /*
-        memory_block_t *cur = free_head;
-        while (cur) {
-            if (is_allocated(cur)) {
-                return -1;
+    memory_block_t *cur = free_head;
+    while (cur) {
+        printf("block address: %p\n", cur);
+        printf("block_size_alloc: %d\n", (int)cur->block_size_alloc);
+        printf("next: %p\n", cur->next);
+        printf("\n");
+        if (is_allocated(cur)) { // marked free
+            return -1;
+        }
+        if(cur->next) {
+            if(&cur > &cur->next) { // address-sorted
+                return -2;
+            }
+            // if memory address + size >= next memory address return -1
+            if( (cur + get_size(cur) ) >= cur->next ) { // checking for overlap
+                return -3;
             }
         }
-    */
+        unsigned int curNum = (uintptr_t) cur;
+        if( curNum % ALIGNMENT != 0) { // 16-byte aligned
+            return -4;
+        }
+        cur = cur->next;
+    }
     return 0;
 }
--- umalloc.c
+++ umalloc.c
@@ -4,7 +4,7 @@
 #include <assert.h>
 #include "ansicolors.h"
 
-const char author[] = ANSI_BOLD ANSI_COLOR_RED "REPLACE THIS WITH YOUR NAME AND UT EID" ANSI_RESET;
+const char author[] = ANSI_BOLD ANSI_COLOR_RED "JAIMIE REN JLR6866" ANSI_RESET;
 
 /*
  * The following helpers can be used to interact with the memory_block_t
@@ -94,7 +94,18 @@ memory_block_t *get_block(void *payload) {
  */
 memory_block_t *find(size_t size) {
     //* STUDENT TODO
-    return NULL;
+    int blockSize = get_size(free_head);
+    memory_block_t *cur = free_head;
+    memory_block_t *res = NULL;
+    while(cur) {
+        int curSize = get_size(cur);
+        if(curSize >= size && curSize < blockSize) {
+            res = cur;
+            blockSize = curSize;
+        }
+        cur = cur->next;
+    }
+    return res;
 }
 
 /*
@@ -102,23 +113,81 @@ memory_block_t *find(size_t size) {
  */
 memory_block_t *extend(size_t size) {
     //* STUDENT TODO
-    return NULL;
+    memory_block_t *res = csbrk(size + ALIGNMENT);
+    res->block_size_alloc = size;
+    if(res < free_head) {
+        res->next = free_head;
+        free_head = res;
+        return coalesce(res);
+    } else {
+        memory_block_t *cur = free_head;
+        while(cur->next) {
+            cur = cur->next;
+        }
+        cur->next = res;
+    }
+    return coalesce(res);
 }
 
 /*
  * split - splits a given block in parts, one allocated, one free.
  */
-memory_block_t *split(memory_block_t *block, size_t size) {
+memory_block_t *split(memory_block_t *block, size_t size) { // get a size sized block from block
     //* STUDENT TODO
-    return NULL;
+    if(block->block_size_alloc - size >= 24) { // split
+        int fullBlockSize = block->block_size_alloc;
+        memory_block_t *res = block;
+        res->block_size_alloc = size;
+        allocate(res);
+        memory_block_t *free;
+        free = block + fullBlockSize + ALIGNMENT;
+        free->block_size_alloc = fullBlockSize - size;
+        ufree(free);
+        return res;
+    }
+    return block; // don't split
 }
 
 /*
  * coalesce - coalesces a free memory block with neighbors.
  */
+// called coalesce at the end of extend, check if the extended heap is connected to original heap
 memory_block_t *coalesce(memory_block_t *block) {
     //* STUDENT TODO
-    return NULL;
+    memory_block_t *prev = free_head;
+    memory_block_t *blockEnd = block + block->block_size_alloc + ALIGNMENT;
+    if(block == prev) { // coalescing at beginning
+        if(prev->next) {
+            if(!is_allocated(prev->next) && blockEnd == prev->next) { // block and next block are adjacent
+                block->block_size_alloc += prev->next->block_size_alloc + ALIGNMENT;
+                block->next = prev->next->next;
+            }
+        }
+    } else {
+        while(prev->next) {
+            if(prev->next == block) { // found the free block
+                memory_block_t *prevEnd = prev + prev->block_size_alloc + ALIGNMENT;
+                if(!is_allocated(prev) && prevEnd == block) { // prev is back to back with block
+                    if(block->next && blockEnd == block->next && !is_allocated(block->next)) { // block is back to back with next so coalesce both sides
+                        prev->block_size_alloc += block->block_size_alloc + block->next->block_size_alloc + (2 * ALIGNMENT);
+                        prev->next = block->next;
+                        return prev;
+                    }  
+                    // only coalesce with prev
+                    prev->block_size_alloc += block->block_size_alloc + ALIGNMENT;
+                    prev->next = block->next;
+                    return prev;
+                }
+                if(block->next && !is_allocated(block->next) && blockEnd == block->next) { // only coalesce with next block
+                    block->block_size_alloc += block->next->block_size_alloc + ALIGNMENT;
+                    block->next = block->next->next;
+                    return block;
+                }
+            }
+            prev = prev->next;
+        }
+    }
+    return block;
 }
 
 
@@ -129,6 +198,14 @@ memory_block_t *coalesce(memory_block_t *block) {
  */
 int uinit() {
     //* STUDENT TODO
+    // use csbrk, any mulitple of pagesize
+    int size = 5 * PAGESIZE;
+    void *ptr = csbrk(size);
+    if(!ptr) {
+        return -1;
+    }
+    free_head = ptr;
+    free_head->block_size_alloc = size - ALIGNMENT;
     return 0;
 }
 
@@ -137,7 +214,27 @@ int uinit() {
  */
 void *umalloc(size_t size) {
     //* STUDENT TODO
-    return NULL;
+    // call find to get free block
+    // 
+    void *bptr = find(size);
+    if(bptr) { // found a block
+        allocate(bptr);
+        if(bptr == free_head) { // allocated block is free_head
+            free_head = free_head->next;
+            return split(bptr, size);
+        }
+        memory_block_t *prev = free_head;
+        while(prev->next) { // allocated block is from middle of list
+            if(prev->next == bptr) {
+                prev->next = prev->next->next;
+                return split(bptr, size);
+            }
+        }
+        prev->next = NULL; // allocated block is last free block
+        return split(bptr, size);
+    }
+    bptr = extend(5 * PAGESIZE);
+    return split(bptr, size);
 }
 
 /*
@@ -146,4 +243,31 @@ void *umalloc(size_t size) {
  */
 void ufree(void *ptr) {
     //* STUDENT TODO
+    // mask and shift bits to get allocated or free indicator bit to check it's allocated 
+    // ^noo use helper methods
+    // loop through linked list and get previous and next block
+    // check if either is free
+    // coalesce (can I call coalesce in here?)
+    memory_block_t *bptr = get_block(ptr);
+    deallocate(bptr);
+    memory_block_t *prev = free_head;
+    if(prev > bptr) { // new free_head
+        bptr->next = free_head;
+        free_head = bptr;
+        coalesce(bptr);
+    } else {
+        while(prev->next) {
+            if(prev->next > bptr) { // insert free block after prev
+                bptr->next = prev->next;
+                prev->next = bptr;
+                coalesce(bptr);
+                return;
+            }
+            prev = prev->next;
+        }
+        prev->next = bptr;
+        bptr->next = NULL;
+        coalesce(bptr);
+    }
+
 }
